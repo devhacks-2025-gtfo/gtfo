32f499f4055fb3c87ed917d37fede4a74dd763cd
lukewiebe <mail@lukewiebe.com>
2025-02-21 15:46:54 -0600

    Feat: add react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import { Theme } from "@radix-ui/themes";
-import StartPage from "./components/StartPage";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Theme accentColor="indigo">
-      <StartPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </BrowserRouter>
     </Theme>
   );
 }
