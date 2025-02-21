deaadec89f65e708d0930da3c5a074bc10ee0737
James Park <parkj6@myumanitoba.ca>
2025-02-21 17:07:33 -0600

    Webgen/everything is in display now

--- Webgen/display.js
+++ Webgen/display.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 4000;
-
+app.use(express.json())
 app.get('/', (req, res) => {
 
     var content = fs.readFileSync('index.html','utf8');
@@ -11,6 +11,11 @@ app.get('/', (req, res) => {
     res.send(content);
 });
 
+app.get('/api/generate', (req, res) => {
+  console.log(req.body["htmlfile"]);
+  fs.writeFileSync("./index.html", req.body["htmlfile"], {encoding: 'utf-8',flag: 'w'});
+  res.send('Generated go to (some link)');
+});
 
 // Start the server
 app.listen(port, () => {
